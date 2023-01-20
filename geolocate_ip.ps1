3be438009c1b9641f34958fbd0936b737848b300
Caner <33795310+acnrayd@users.noreply.github.com>
2023-01-20 17:23:49 +0300

    Parsing IP's by country

---
+++ geolocate_ip.ps1
@@ -0,0 +1,28 @@
+# Following PS script will read a file, parse IP addresses, remove duplicate entries, gets country of these IP's and retrieves them as a table.
+# Usage: run this script by provides file includes IP addresses as parameter
+
+Param(
+  [parameter(mandatory=$true)][string]$filename
+)
+
+$filename = Get-Content $filename
+$file = $filename | sort -unique
+$regex = "\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+$matches = [regex]::Matches($file, $regex)
+
+$records = @()
+
+
+foreach ($match in $matches) {
+    $ip = $match.Value
+    $country = (Invoke-WebRequest "http://ip-api.com/json/$ip" -UseBasicParsing).Content | ConvertFrom-Json
+    Write-Host "IP: $ip  Country: $($country.country)" 
+
+    $records += New-Object PSObject -Property @{
+        IP = $ip
+        Country = $($country.country)
+}
+}
+
+$records | Out-GridView -Title "IP by Country"
+ 
